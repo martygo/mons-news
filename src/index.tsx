61f150d1387d21453743694d9c9f84ad7ddb886b
Martins Gouveia <gouveiamartins6@gmail.com>
2022-12-14 19:53:34 +0100

    🔧 ➕ config react-query

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./App.css";
+import { QueryClient, QueryClientProvider } from "react-query";
+
 import App from "./App";
+import "./App.css";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
 	document.getElementById("root") as HTMLElement,
 );
 root.render(
 	<React.StrictMode>
-		<App />
+		<QueryClientProvider client={queryClient}>
+			<App />
+		</QueryClientProvider>
 	</React.StrictMode>,
 );
 
